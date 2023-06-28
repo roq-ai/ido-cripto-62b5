0b49ff31a9f5b3f10380e156dd6f29d015ba33de
roq-ai <>
2023-06-28 01:20:44 +0000

    Initial commit

---
+++ src/interfaces/organization/index.ts
@@ -0,0 +1,28 @@
+import { ArticleInterface } from 'interfaces/article';
+import { UserInterface } from 'interfaces/user';
+import { GetQueryInterface } from 'interfaces';
+
+export interface OrganizationInterface {
+  id?: string;
+  description?: string;
+  image?: string;
+  name: string;
+  created_at?: any;
+  updated_at?: any;
+  user_id: string;
+  tenant_id: string;
+  article?: ArticleInterface[];
+  user?: UserInterface;
+  _count?: {
+    article?: number;
+  };
+}
+
+export interface OrganizationGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  description?: string;
+  image?: string;
+  name?: string;
+  user_id?: string;
+  tenant_id?: string;
+}
